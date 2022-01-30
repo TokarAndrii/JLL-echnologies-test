712a4e516f5d471e2f91b2a2bf32c6e9d1373546
tokarandrii <tokar.andrii@gmail.com>
2022-01-30 12:12:29 +0200

    add redirect to list of tenants from root

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Typography from "@material-ui/core/Typography";
 import TenantsList from "./pages/TenantsList";
 import Tenant from "./pages/Tenant";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -10,7 +10,9 @@ function App() {
       <Typography variant="h4" component="h2">
         JJL Technologies app
       </Typography>
+
       <Routes>
+        <Route path="*" element={<Navigate to="/list" />} />
         <Route path="/list" element={<TenantsList />} />
         <Route path="list/:id" element={<Tenant />} />
       </Routes>
